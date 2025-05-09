ee3e53ea3ddd295bb1eabece94d194c6efcaba83
imstellar <182440248+imstellar@users.noreply.github.com>
2025-05-09 13:29:33 +0100

    Initial Project commit - Contains all base project files - Changed theme.kt and color.kt based on colour scheme

---
+++ app/src/main/java/com/assignment/habitally/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.assignment.habitally.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val appPrimary80 = Color(0xFFFFB68D)
+val appSecondary80 = Color(0xFFEABDA6)
+val appTertiary80 = Color(0xFFCDC989)
+
+val appPrimary40 = Color(0xFF9B4600)
+val appSecondary40 = Color(0xFF795744)
+val appTertiary40 = Color(0xFF63602B)
+
